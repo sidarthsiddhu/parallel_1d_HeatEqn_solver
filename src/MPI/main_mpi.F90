3b1d3ae3a64e5ac41de2ef27fae33befaa84a434
Sidarth Narayanan <snarayanan1@USLN38.prog.altair.com>
2022-12-01 13:33:22 -0500

    Updated Main.F90
    Works with 1 proc and fails with more than 1 proc(s). Error related to array bounds/misuse

--- src/MPI/main_mpi.F90
+++ src/MPI/main_mpi.F90
@@ -9,13 +9,13 @@ Program Main
 
     Integer(kind=8) :: npts,ntime,it,i,npts_local
     Integer(kind=8) :: remeinder,start_indx,end_indx
-    Real,Allocatable,Dimension(:) :: T_old,T_new,T_exact
-    Real,Allocatable,Dimension(:) :: x,t
+    Real(kind=8),Allocatable,Dimension(:) :: T_old,T_new,T_exact
+    Real(kind=8),Allocatable,Dimension(:) :: x,t
     Real(kind=8) :: Total_time,length,dx,dt,alpha,r
     Real(kind=8) :: T_left,T_right,T_initial
     Real(kind=8) :: ghost_left,ghost_right
     Integer :: request_left,request_right,ierr,myid,nprocs,debug_version
-    Integer,Dimension(MPI_STATUS_SIZE) :: status_left,status_right
+    Integer,Dimension(MPI_STATUS_SIZE) :: status
 
 ! Initialize MPI and call rank and size functions
     Call MPI_INIT(ierr)
@@ -23,18 +23,35 @@ Program Main
     Call MPI_COMM_RANK(MPI_COMM_WORLD, myid  , ierr)
 
 ! Set the problem parameters
-    debug_version = 1
+    debug_version = 0
     length = 1.0
-    Total_time = 10.0
+    Total_time = 1.0
     npts = 10
     ntime = 1
-    alpha = 1.0
+    alpha = 0.0001
     T_left = 0.0 
     T_right = 100.0
     T_initial = 25.0
     dx = length/real(npts)
     dt = Total_time/real(ntime)
     r = (alpha*dt)/(dx*dx)
+	
+	If(myid .EQ. 0) then
+		write(6,'(a75)') " --- Parallel 1-D Heat Equation Solver [dT/dt = alpha*(d2T/dx2)] --- "
+		write(6,'(a75)') " --------------------- Problem Parameters -------------------------- "
+		write(6,'(a34,F12.4,a)') " Length: ",length," meters"
+		write(6,'(a34,F12.4,a)') " Total simulation time: ",Total_time," seconds"
+		write(6,'(a34,ES12.5,a)') " dt: ",dt," seconds"
+		write(6,'(a34,ES12.5,a)') " dx: ",dx," meters"
+		write(6,'(a34,I0,a)') " Number of points (spatial): ",npts," Point(s)"
+		write(6,'(a34,I0,a)') " Number of time steps : ",ntime," Step(s)"
+		write(6,'(a34,ES12.5,a)') " Thermal Diffusivity: ",alpha," m^2/s"
+		write(6,'(a34,ES12.5)') " R(alpha*dt/dx^2) : ",r
+		write(6,'(a34,I0,a)') " Number of Processors : ",nprocs," Processor(s)"
+		write(6,*) ""
+		Call flush(6)
+	Endif
+	Call MPI_Barrier(MPI_COMM_WORLD,ierr)
 
 ! Split the domain among the processors in a load balanced method 
     npts_local = npts/nprocs
@@ -46,11 +63,6 @@ Program Main
     Else
         start_indx = (myid * npts_local) + remeinder + 1
         end_indx = start_indx + npts_local - 1
-    Endif
-
-    If(debug_version .EQ. 1) then
-        write(6,'(a,I0,a,I0,a,I0)') "My ID :",myid,"  ||  Start Index:",start_indx,"  ||  End Index:",end_indx
-        call flush(6)
     Endif
 
 ! Allocate the variables
@@ -65,98 +77,64 @@ Program Main
         T_new(i) = T_initial + i
         x(i) = (i-1)*dx
     Enddo
-
+	t(1) = 0.0 ! Start with time 0.0 seconds
 ! Set boundary condition
     If(myid .EQ. 0) T_new(start_indx) = T_left
     If(myid .EQ. (nprocs-1)) T_new(end_indx) = T_right
 	
-	If(debug_version .EQ. 1) then
-        If(myid .EQ. 0) write(6,*) "-------------- Initial Condition ---------------"
-            Do i=start_indx,end_indx
-                write(6,*) "T(",i,") = ",T_new(i)
-            Enddo
-            Call flush(6)
-    Endif
-
-    Do it = 1,ntime
+    Do it = 2,ntime
     
         Call MPI_Barrier(MPI_COMM_WORLD,ierr)
-		!If(debug_version .EQ. 1) then
-		!	write(6,'(a,I0,a)') "Myid :",myid,"  || Inside Loop after MPI_Barrier"
-		!	call flush(6)
-		!Endif
+		t(it) = t(it-1) + dt
 		Do i=start_indx,end_indx
 			T_old(i) = T_new(i)
 		Enddo
-        If(myid .EQ. 0) then
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid+1, myid  , MPI_COMM_WORLD, ierr)
-			!Call MPI_Recv(ghost_right    , 1, MPI_DOUBLE, myid+1, myid+1, MPI_COMM_WORLD, ierr)
-            Call MPI_Isend(T_new(end_indx),1,MPI_DOUBLE,myid+1,myid,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Irecv(ghost_right,1,MPI_DOUBLE,myid+1,myid+1,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Wait(request_right,status_right,ierr)
-			If(debug_version .EQ. 1) then
-				write(6,'(a,I0,a)') "Myid :",myid,"  || Completed send and receive"
-				call flush(6)
-			Endif
-        Elseif(myid .EQ. (nprocs-1)) then
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid-1, myid  , MPI_COMM_WORLD, ierr)
-            !Call MPI_Recv(ghost_left	 , 1, MPI_DOUBLE, myid-1, myid-1, MPI_COMM_WORLD, ierr)
-            Call MPI_Isend(T_new(end_indx),1,MPI_DOUBLE,myid-1,myid,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Irecv(ghost_left,1,MPI_DOUBLE,myid-1,myid-1,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Wait(request_left,status_left,ierr)
-			If(debug_version .EQ. 1) then
-				write(6,'(a,I0,a)') "Myid :",myid,"  || Completed send and receive"
-				call flush(6)
-			Endif
-        Else
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid+1, myid  , MPI_COMM_WORLD, ierr)
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid-1, myid  , MPI_COMM_WORLD, ierr)
-			!Call MPI_Recv(ghost_right    , 1, MPI_DOUBLE, myid+1, myid+1, MPI_COMM_WORLD, ierr)
-            !Call MPI_Recv(ghost_left	 , 1, MPI_DOUBLE, myid-1, myid-1, MPI_COMM_WORLD, ierr)
-            Call MPI_Isend(T_new(start_indx),1,MPI_DOUBLE,myid-1,myid,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Isend(T_new(end_indx),1,MPI_DOUBLE,myid+1,myid,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Irecv(ghost_right,1,MPI_DOUBLE,myid+1,myid+1,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Irecv(ghost_left,1,MPI_DOUBLE,myid-1,myid-1,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Wait(request_right,status_right,ierr)
-            Call MPI_Wait(request_left,status_left,ierr)
-			If(debug_version .EQ. 1) then
-				write(6,'(a,I0,a)') "Myid :",myid,"  || Completed send and receive"
-				call flush(6)
-			Endif
-        Endif
-		call flush(6)
-		Call MPI_Barrier(MPI_COMM_WORLD,ierr)
 		
-        If(myid .EQ. 0) then
-            Do i=start_indx+1,end_indx
-                If(i .EQ. end_indx) then
-                    T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*ghost_right
-                Else
-                    T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
-                Endif
-            Enddo
-        Elseif(myid .EQ. (nprocs-1)) then
-            Do i=start_indx,end_indx-1
-                If(i .EQ. start_indx) then
-                    T_new(i) = r*ghost_left + (1-(2*r))*T_old(i) + r*T_old(i+1)
-                Else
-                    T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
-                Endif
-            Enddo
-        Else
-            Do i=start_indx,end_indx
-                T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
-            Enddo
-        Endif
+		If(nprocs .GT. 1) then
+			If(myid .EQ. 0) then
+				Call MPI_Sendrecv(T_new(end_indx),1,MPI_DOUBLE_PRECISION,myid+1,myid,ghost_right,1,MPI_DOUBLE_PRECISION,myid+1,myid+1,MPI_COMM_WORLD,status,ierr)
+			Elseif(myid .EQ. (nprocs-1)) then
+				Call MPI_Sendrecv(T_new(start_indx),1,MPI_DOUBLE_PRECISION,myid-1,myid,ghost_left,1,MPI_DOUBLE_PRECISION,myid-1,myid-1,MPI_COMM_WORLD,status,ierr)
+			Else
+				Call MPI_Sendrecv(T_new(end_indx),1,MPI_DOUBLE_PRECISION,myid+1,myid,ghost_right,1,MPI_DOUBLE_PRECISION,myid+1,myid+1,MPI_COMM_WORLD,status,ierr)
+				Call MPI_Sendrecv(T_new(start_indx),1,MPI_DOUBLE_PRECISION,myid-1,myid,ghost_left,1,MPI_DOUBLE_PRECISION,myid-1,myid-1,MPI_COMM_WORLD,status,ierr)
+			Endif
 		
+			If(myid .EQ. 0) then
+				Do i=start_indx+1,end_indx
+					If(i .EQ. end_indx) then
+						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*ghost_right
+					Else
+						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+					Endif
+				Enddo
+			Elseif(myid .EQ. (nprocs-1)) then
+				Do i=start_indx,end_indx-1
+					If(i .EQ. start_indx) then
+						T_new(i) = r*ghost_left + (1-(2*r))*T_old(i) + r*T_old(i+1)
+					Else
+						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+					Endif
+				Enddo
+			Else
+				Do i=start_indx,end_indx
+					T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+				Enddo
+			Endif
+		Else
+			Do i=start_indx+1,end_indx-1
+				T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+			Enddo
+		Endif
+			
     Enddo
 
     Call MPI_Barrier(MPI_COMM_WORLD,ierr)
 
     If(debug_version .EQ. 1) then
-        If(myid .EQ. 0) write(6,*) "-------------- Final Answer ---------------"
+        If(myid .EQ. 0) write(6,'(a75)') " --------------------------- Final Answer -------------------------- "
             Do i=start_indx,end_indx
-                write(6,*) "T(",i,") = ",T_new(i)
+                write(6,'(a34,I0,a,F12.4)') "T(",i,") = ",T_new(i)
             Enddo
             Call flush(6)
     Endif
@@ -166,7 +144,7 @@ Program Main
     Deallocate(T_exact)
     Deallocate(x)
     Deallocate(t)
-
+	
     Call MPI_FINALIZE(ierr)
 
 End Program Main
