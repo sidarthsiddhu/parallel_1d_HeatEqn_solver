85f6218941ca12cb4497af8376c5d0c0f23ef050
Sidarth Narayanan <sid1071998@gmail.com>
2022-12-04 21:51:05 -0500

    Finsihed the openMP version

--- src/OpenMP/main_OMP.F90
+++ src/OpenMP/main_OMP.F90
@@ -1,34 +1,28 @@
-! ----------- ECE 563 Programming Parallel Machines --------------!
+! ----------------- ECE 563 Programming Parallel Machines ------------------!
 ! ----------- Final Project : Parallel 1-D Heat Equation Solver ----------- !
-! ----------- Author: Sidarth Narayanan ----------- !
+! ----------------------------- Version : OpenMP ---------------------------!
+! ------------------------ Author: Sidarth Narayanan ---------------------- !
 
 Program Main
-
-    Use MPI
+	
+	USE OMP_LIB
     Implicit None
 
-    Integer(kind=8) :: npts,ntime,it,i,npts_local
-    Integer(kind=8) :: remeinder,start_indx,end_indx
+    Integer(kind=8) :: npts,ntime,it,i
     Real,Allocatable,Dimension(:) :: T_old,T_new,T_exact
     Real,Allocatable,Dimension(:) :: x,t
     Real(kind=8) :: Total_time,length,dx,dt,alpha,r
     Real(kind=8) :: T_left,T_right,T_initial
-    Real(kind=8) :: ghost_left,ghost_right
     Integer :: request_left,request_right,ierr,myid,nprocs,debug_version
-    Integer,Dimension(MPI_STATUS_SIZE) :: status_left,status_right
-
-! Initialize MPI and call rank and size functions
-    Call MPI_INIT(ierr)
-    Call MPI_COMM_SIZE(MPI_COMM_WORLD, nprocs, ierr)
-    Call MPI_COMM_RANK(MPI_COMM_WORLD, myid  , ierr)
+	Real(kind=8) :: time_taken
 
 ! Set the problem parameters
-    debug_version = 1
+    debug_version = 0
     length = 1.0
-    Total_time = 10.0
-    npts = 10
-    ntime = 1
-    alpha = 1.0
+    Total_time = 1.0
+    npts = 1000000
+    ntime = 100000
+    alpha = 0.001
     T_left = 0.0 
     T_right = 100.0
     T_initial = 25.0
@@ -36,129 +30,55 @@ Program Main
     dt = Total_time/real(ntime)
     r = (alpha*dt)/(dx*dx)
 
-! Split the domain among the processors in a load balanced method 
-    npts_local = npts/nprocs
-    remeinder = mod(npts,nprocs)
-    If(myid .LT. remeinder) then
-        npts_local = npts_local + 1
-        start_indx = (myid*npts_local) + 1
-        end_indx = start_indx + npts_local - 1
-    Else
-        start_indx = (myid * npts_local) + remeinder + 1
-        end_indx = start_indx + npts_local - 1
-    Endif
-
-    If(debug_version .EQ. 1) then
-        write(6,'(a,I0,a,I0,a,I0)') "My ID :",myid,"  ||  Start Index:",start_indx,"  ||  End Index:",end_indx
-        call flush(6)
-    Endif
-
 ! Allocate the variables
-    Allocate(T_old(npts_local))
-    Allocate(T_new(npts_local))
-    Allocate(T_exact(npts_local))
-    Allocate(x(npts_local))
+    Allocate(T_old(npts))
+    Allocate(T_new(npts))
+    Allocate(T_exact(npts))
+    Allocate(x(npts))
     Allocate(t(ntime))
 
 ! Set initial condition
-    Do i=start_indx,end_indx
+    Do i=1,npts
         T_new(i) = T_initial + i
         x(i) = (i-1)*dx
     Enddo
 
 ! Set boundary condition
-    If(myid .EQ. 0) T_new(start_indx) = T_left
-    If(myid .EQ. (nprocs-1)) T_new(end_indx) = T_right
+	T_new(1) = T_left
+    T_new(npts) = T_right
 	
 	If(debug_version .EQ. 1) then
-        If(myid .EQ. 0) write(6,*) "-------------- Initial Condition ---------------"
-            Do i=start_indx,end_indx
-                write(6,*) "T(",i,") = ",T_new(i)
-            Enddo
-            Call flush(6)
+        write(6,*) "-------------- Initial Condition ---------------"
+        Do i=1,npts
+            write(6,*) "T(",i,") = ",T_new(i)
+        Enddo
     Endif
-
+	
+	time_taken = -1.0*omp_get_wtime()
     Do it = 1,ntime
-    
-        Call MPI_Barrier(MPI_COMM_WORLD,ierr)
-		!If(debug_version .EQ. 1) then
-		!	write(6,'(a,I0,a)') "Myid :",myid,"  || Inside Loop after MPI_Barrier"
-		!	call flush(6)
-		!Endif
-		Do i=start_indx,end_indx
+		
+		!$OMP PARALLEL DO PRIVATE(i)
+		Do i=2,npts-1
 			T_old(i) = T_new(i)
 		Enddo
-        If(myid .EQ. 0) then
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid+1, myid  , MPI_COMM_WORLD, ierr)
-			!Call MPI_Recv(ghost_right    , 1, MPI_DOUBLE, myid+1, myid+1, MPI_COMM_WORLD, ierr)
-            Call MPI_Isend(T_new(end_indx),1,MPI_DOUBLE,myid+1,myid,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Irecv(ghost_right,1,MPI_DOUBLE,myid+1,myid+1,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Wait(request_right,status_right,ierr)
-			If(debug_version .EQ. 1) then
-				write(6,'(a,I0,a)') "Myid :",myid,"  || Completed send and receive"
-				call flush(6)
-			Endif
-        Elseif(myid .EQ. (nprocs-1)) then
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid-1, myid  , MPI_COMM_WORLD, ierr)
-            !Call MPI_Recv(ghost_left	 , 1, MPI_DOUBLE, myid-1, myid-1, MPI_COMM_WORLD, ierr)
-            Call MPI_Isend(T_new(end_indx),1,MPI_DOUBLE,myid-1,myid,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Irecv(ghost_left,1,MPI_DOUBLE,myid-1,myid-1,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Wait(request_left,status_left,ierr)
-			If(debug_version .EQ. 1) then
-				write(6,'(a,I0,a)') "Myid :",myid,"  || Completed send and receive"
-				call flush(6)
-			Endif
-        Else
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid+1, myid  , MPI_COMM_WORLD, ierr)
-			!Call MPI_Send(T_new(end_indx), 1, MPI_DOUBLE, myid-1, myid  , MPI_COMM_WORLD, ierr)
-			!Call MPI_Recv(ghost_right    , 1, MPI_DOUBLE, myid+1, myid+1, MPI_COMM_WORLD, ierr)
-            !Call MPI_Recv(ghost_left	 , 1, MPI_DOUBLE, myid-1, myid-1, MPI_COMM_WORLD, ierr)
-            Call MPI_Isend(T_new(start_indx),1,MPI_DOUBLE,myid-1,myid,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Isend(T_new(end_indx),1,MPI_DOUBLE,myid+1,myid,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Irecv(ghost_right,1,MPI_DOUBLE,myid+1,myid+1,MPI_COMM_WORLD,request_right,ierr)
-            Call MPI_Irecv(ghost_left,1,MPI_DOUBLE,myid-1,myid-1,MPI_COMM_WORLD,request_left,ierr)
-            Call MPI_Wait(request_right,status_right,ierr)
-            Call MPI_Wait(request_left,status_left,ierr)
-			If(debug_version .EQ. 1) then
-				write(6,'(a,I0,a)') "Myid :",myid,"  || Completed send and receive"
-				call flush(6)
-			Endif
-        Endif
-		call flush(6)
-		Call MPI_Barrier(MPI_COMM_WORLD,ierr)
+		!$OMP END PARALLEL DO
 		
-        If(myid .EQ. 0) then
-            Do i=start_indx+1,end_indx
-                If(i .EQ. end_indx) then
-                    T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*ghost_right
-                Else
-                    T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
-                Endif
-            Enddo
-        Elseif(myid .EQ. (nprocs-1)) then
-            Do i=start_indx,end_indx-1
-                If(i .EQ. start_indx) then
-                    T_new(i) = r*ghost_left + (1-(2*r))*T_old(i) + r*T_old(i+1)
-                Else
-                    T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
-                Endif
-            Enddo
-        Else
-            Do i=start_indx,end_indx
-                T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
-            Enddo
-        Endif
+		!$OMP PARALLEL DO PRIVATE(i)
+        Do i=2,npts-1
+            T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+        Enddo
+		!$OMP END PARALLEL DO
 		
     Enddo
-
-    Call MPI_Barrier(MPI_COMM_WORLD,ierr)
-
+	time_taken = time_taken + omp_get_wtime()
+	
+	write(6,'(a,F12.5,a)') "Time taken to solve: ",time_taken," seconds"
+	
     If(debug_version .EQ. 1) then
-        If(myid .EQ. 0) write(6,*) "-------------- Final Answer ---------------"
-            Do i=start_indx,end_indx
-                write(6,*) "T(",i,") = ",T_new(i)
-            Enddo
-            Call flush(6)
+        write(6,*) "-------------- Final Answer ---------------"
+        Do i=1,npts
+            write(6,*) "T(",i,") = ",T_new(i)
+        Enddo
     Endif
 
     Deallocate(T_old)
@@ -166,7 +86,5 @@ Program Main
     Deallocate(T_exact)
     Deallocate(x)
     Deallocate(t)
-
-    Call MPI_FINALIZE(ierr)
 
 End Program Main
