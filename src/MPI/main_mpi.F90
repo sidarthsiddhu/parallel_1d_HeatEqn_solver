c666fc6c2ba4bd9a04b3bae58a73167ae4f8ae8e
Sidarth Narayanan <sid1071998@gmail.com>
2022-12-05 00:20:13 -0500

    Working MPI Code

--- src/MPI/main_mpi.F90
+++ src/MPI/main_mpi.F90
@@ -8,14 +8,13 @@ Program Main
     Implicit None
 
     Integer(kind=8) :: npts,ntime,it,i,npts_local
-    Integer(kind=8) :: remeinder,start_indx,end_indx
-    Real(kind=8),Allocatable,Dimension(:) :: T_old,T_new,T_exact
-    Real(kind=8),Allocatable,Dimension(:) :: x,t
+    Integer(kind=8) :: start_indx,end_indx
+    Real(kind=8),Allocatable,Dimension(:) :: T_old,T_new
     Real(kind=8) :: Total_time,length,dx,dt,alpha,r
-    Real(kind=8) :: T_left,T_right,T_initial
-    Real(kind=8) :: ghost_left,ghost_right
-    Integer :: request_left,request_right,ierr,myid,nprocs,debug_version
-    Integer,Dimension(MPI_STATUS_SIZE) :: status
+    Real(kind=8) :: T_left,T_right,T_initial,T_exact,error,error_max
+    Real(kind=8) :: ghost_left,ghost_right,time_start,time_end,time_elapsed
+    Integer :: ierr,myid,nprocs,debug_version,remeinder
+    Integer,Dimension(MPI_STATUS_SIZE) :: status_right,status_left
 
 ! Initialize MPI and call rank and size functions
     Call MPI_INIT(ierr)
@@ -24,17 +23,20 @@ Program Main
 
 ! Set the problem parameters
     debug_version = 0
-    length = 1.0
-    Total_time = 1.0
-    npts = 10
-    ntime = 1
-    alpha = 0.0001
+    length = 10.0
+    Total_time = 10.0
+    npts = 10000
+    alpha = 5.0
     T_left = 0.0 
-    T_right = 100.0
-    T_initial = 25.0
-    dx = length/real(npts)
-    dt = Total_time/real(ntime)
-    r = (alpha*dt)/(dx*dx)
+    T_right = 0.0
+    T_initial = 50.0
+	T_exact = 0.0
+	error = 0.0
+	r = 0.5
+	dx = length/real(npts)
+	dt = r*(dx**2)/(alpha)
+	ntime = CEILING(Total_time/dt)
+    
 	
 	If(myid .EQ. 0) then
 		write(6,'(a75)') " --- Parallel 1-D Heat Equation Solver [dT/dt = alpha*(d2T/dx2)] --- "
@@ -64,45 +66,67 @@ Program Main
         start_indx = (myid * npts_local) + remeinder + 1
         end_indx = start_indx + npts_local - 1
     Endif
+	
+! Check npts_local and indices
+	If(debug_version .EQ. 1) then
+		write(6,'(a,I0,a,I0,a,I0,a,I0)') "Id: ",myid," || Start: ",start_indx," || End: ",end_indx," || npts_local: ",npts_local
+	Endif
 
 ! Allocate the variables
-    Allocate(T_old(npts_local))
-    Allocate(T_new(npts_local))
-    Allocate(T_exact(npts_local))
-    Allocate(x(npts_local))
-    Allocate(t(ntime))
+    Allocate(T_old(start_indx:end_indx))
+    Allocate(T_new(start_indx:end_indx))
 
 ! Set initial condition
     Do i=start_indx,end_indx
-        T_new(i) = T_initial + i
-        x(i) = (i-1)*dx
+        T_new(i) = T_initial
     Enddo
-	t(1) = 0.0 ! Start with time 0.0 seconds
+	
 ! Set boundary condition
     If(myid .EQ. 0) T_new(start_indx) = T_left
     If(myid .EQ. (nprocs-1)) T_new(end_indx) = T_right
 	
+! Calculate initial RMS error from exact solution
+	Do i = start_indx,end_indx
+		error = error + (T_new(i) - T_exact)**2
+	Enddo
+	error = SQRT(error)/npts_local
+	Call MPI_Reduce(error,error_max,1,MPI_DOUBLE_PRECISION,MPI_MAX,0,MPI_COMM_WORLD,ierr)
+	If(myid .EQ. 0) write(6,'(a,f12.4)') "The Initial RMS Error is: ",error_max
+	
+! Reset Error value for final calculation
+	error = 0.0
+
+	If(myid .EQ. 0) write(6,'(a)') "Starting Calculation ... "
+
+! Start the time counter
+	time_start = MPI_Wtime()
+
+! Start the time iteration
     Do it = 2,ntime
     
-        Call MPI_Barrier(MPI_COMM_WORLD,ierr)
-		t(it) = t(it-1) + dt
+	! Set T_old = T_new
 		Do i=start_indx,end_indx
 			T_old(i) = T_new(i)
 		Enddo
-		
+
+	! Pass the details of the ghostg points to the neighbours		
 		If(nprocs .GT. 1) then
-			If(myid .EQ. 0) then
-				Call MPI_Sendrecv(T_new(end_indx),1,MPI_DOUBLE_PRECISION,myid+1,myid,ghost_right,1,MPI_DOUBLE_PRECISION,myid+1,myid+1,MPI_COMM_WORLD,status,ierr)
-			Elseif(myid .EQ. (nprocs-1)) then
-				Call MPI_Sendrecv(T_new(start_indx),1,MPI_DOUBLE_PRECISION,myid-1,myid,ghost_left,1,MPI_DOUBLE_PRECISION,myid-1,myid-1,MPI_COMM_WORLD,status,ierr)
-			Else
-				Call MPI_Sendrecv(T_new(end_indx),1,MPI_DOUBLE_PRECISION,myid+1,myid,ghost_right,1,MPI_DOUBLE_PRECISION,myid+1,myid+1,MPI_COMM_WORLD,status,ierr)
-				Call MPI_Sendrecv(T_new(start_indx),1,MPI_DOUBLE_PRECISION,myid-1,myid,ghost_left,1,MPI_DOUBLE_PRECISION,myid-1,myid-1,MPI_COMM_WORLD,status,ierr)
-			Endif
 		
+		! Send the left most element
+			If(myid .GT. 0)	Call MPI_Send(T_new(start_indx),1,MPI_DOUBLE_PRECISION,myid-1,1,MPI_COMM_WORLD,ierr)
+		! Recieve the right ghost element
+			If(myid .LT. nprocs-1) Call MPI_Recv(ghost_right,1,MPI_DOUBLE_PRECISION,myid+1,1,MPI_COMM_WORLD,status_right,ierr)
+			
+		! Send the right most element	
+			If(myid .LT. nprocs-1)	Call MPI_Send(T_new(end_indx),1,MPI_DOUBLE_PRECISION,myid+1,1,MPI_COMM_WORLD,ierr)
+		! Recieve the left ghost element
+			If(myid .GT. 0) Call MPI_Recv(ghost_left,1,MPI_DOUBLE_PRECISION,myid-1,1,MPI_COMM_WORLD,status_left,ierr)
+		
+		! Do the actual calculation point by point
 			If(myid .EQ. 0) then
 				Do i=start_indx+1,end_indx
 					If(i .EQ. end_indx) then
+					! End point uses right ghost node	
 						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*ghost_right
 					Else
 						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
@@ -111,6 +135,7 @@ Program Main
 			Elseif(myid .EQ. (nprocs-1)) then
 				Do i=start_indx,end_indx-1
 					If(i .EQ. start_indx) then
+					! Start point uses left ghost node
 						T_new(i) = r*ghost_left + (1-(2*r))*T_old(i) + r*T_old(i+1)
 					Else
 						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
@@ -118,7 +143,15 @@ Program Main
 				Enddo
 			Else
 				Do i=start_indx,end_indx
-					T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+					If(I .EQ. start_indx) then
+					! Start point uses left ghost node
+						T_new(i) = r*ghost_left + (1-(2*r))*T_old(i) + r*T_old(i+1)
+					Elseif(i .EQ. end_indx) then
+					! End point uses right ghost node
+						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*ghost_right
+					Else
+						T_new(i) = r*T_old(i-1) + (1-(2*r))*T_old(i) + r*T_old(i+1)
+					Endif
 				Enddo
 			Endif
 		Else
@@ -128,23 +161,40 @@ Program Main
 		Endif
 			
     Enddo
-
+	
     Call MPI_Barrier(MPI_COMM_WORLD,ierr)
+	
+! Stop time count and calculate elapsed time
+	time_end = MPI_Wtime()
+	time_elapsed = time_end - time_start
+	
+	If(myid .EQ. 0) write(6,'(a)') "Calculation Complete"
+	
+! Calculate error from exact solution to check the results
+	Do i = start_indx,end_indx
+		error = error + (T_new(i) - T_exact)**2
+	Enddo
+	error = SQRT(error)/npts_local
+	Call MPI_Reduce(error,error_max,1,MPI_DOUBLE_PRECISION,MPI_MAX,0,MPI_COMM_WORLD,ierr)
 
+! Print out elapsed time
+	If(myid .EQ. 0) then
+		write(6,'(a,f12.4)') "The RMS Error is: ",error_max
+		write(6,'(a,f12.4,a)') "The Elapsed time is: ",time_elapsed," seconds"
+	Endif
+	
     If(debug_version .EQ. 1) then
         If(myid .EQ. 0) write(6,'(a75)') " --------------------------- Final Answer -------------------------- "
             Do i=start_indx,end_indx
                 write(6,'(a34,I0,a,F12.4)') "T(",i,") = ",T_new(i)
             Enddo
-            Call flush(6)
     Endif
 
+! Deallocate Variables	
     Deallocate(T_old)
     Deallocate(T_new)
-    Deallocate(T_exact)
-    Deallocate(x)
-    Deallocate(t)
-	
-    Call MPI_FINALIZE(ierr)
+   
+! Finalize MPI
+	Call MPI_FINALIZE(ierr)
 
 End Program Main
